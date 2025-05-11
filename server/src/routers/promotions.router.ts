cc0126792e627bf24c0a74a1edcdb30244e9f0dc
huyphantom <huyphantom2004@gmail.com>
2025-05-11 18:59:16 +0700

    a

--- server/src/routers/promotions.router.ts
+++ server/src/routers/promotions.router.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
-import * as promotionManager from '../controllers/managers/promotions.controller';
+// import { Router } from 'express';
+// import * as promotionManager from '../controllers/managers/promotions.controller';
 
-const router = Router();
+// const router = Router();
 
-router.get('/product-promotions', promotionManager.getPromotion);
-router.post('/product-promotions', promotionManager.createPromotion);
-router.put('/product-promotions/:productId/:promotionId', promotionManager.updatePromotion);
-router.delete('/product-promotions/:productId/:promotionId', promotionManager.deletePromotion);
+// router.get('/product-promotions', promotionManager.getPromotion);
+// router.post('/product-promotions', promotionManager.createPromotion);
+// router.put('/product-promotions/:productId/:promotionId', promotionManager.updatePromotion);
+// router.delete('/product-promotions/:productId/:promotionId', promotionManager.deletePromotion);
